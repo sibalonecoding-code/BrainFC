a99e002d61c1e25538d23ed4ca73f11553a8e060
Âne Mou <sibalonecoding@gmail.com>
2025-12-26 12:18:31 +0100

    màj deckmanager, créer les dossiers inexistant

--- scripts/models/deckmanager.py
+++ scripts/models/deckmanager.py
@@ -16,10 +16,15 @@ class DeckManager:
         self.data_folder = kwargs.get("data_folder", "data/")
         self.decks: dict[str, Deck] = kwargs.get("decks", dict())
 
+    def data_folder_exists(self) -> bool:
+        return os.path.exists(self.data_folder)
+
     def deck_exists(self, name:str) -> bool:
         return name in self.scan_decks()
 
     def scan_decks(self) -> list[str]:
+        if not self.data_folder_exists():
+            os.makedirs(self.data_folder)
         return [item for item in os.listdir(self.data_folder) if item.endswith(".json")]
 
     def get_deck(self, name:str) -> Deck:
